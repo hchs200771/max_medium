2a0840ed1062cbdde62df77997607ba635c3332d
max_huang <hchs200771@gmail.com>
2020-05-01 19:33:46 +0800

    實作首頁

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   }
 
   root 'pages#index'
+  # /@maxharry/文章標題
+  get '@:user_name/:story_id', to: 'pages#show', as: :story_page
+  # /@maxharry/   顯示該作者的所有文章
+  get '@:user_name', to: 'pages#user', as: :user_page
+
   resources :stories
 end
